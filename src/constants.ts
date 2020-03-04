b6c3fc95c50da2725a085950b50f48e74b6a18cf
cdr <qualitymanifest@gmail.com>
2020-03-03 23:46:57 -0500

    Remove colors from error messages

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,8 @@
 import "colors";
 
-export const NO_PATH_ERR = "Please provide a file or directory to start from"
-  .red.bold;
-export const BAD_PATH_ERR = "The starting path provided could not be resolved"
-  .red.bold;
-export const NO_EXT_ERR = "Please provide a list of file extensions to search for"
-  .red.bold;
-export const MIN_TYPE_ERR = "Min must be a number".red.bold;
+export const NO_PATH_ERR = "Please provide a file or directory to start from";
+export const BAD_PATH_ERR = "The starting path provided could not be resolved";
+export const NO_EXT_ERR =
+  "Please provide a list of file extensions to search for";
+export const MIN_TYPE_ERR = "Min must be a number";
 export const DEFAULT_IGNORE = ["node_modules", ".git"];
